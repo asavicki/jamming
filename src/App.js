d75d42f239cb0d78c788d8126e268ced47adefcc
asavickij <asavickij@gmail.com>
2024-08-15 14:16:04 +0300

    Hardcoded the track array of objects and passed through to Track component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import Tracklist from './components/Tracklist';
+import Track from './components/Track';
 
 function App() {
+  const [tracks, setTracks] = useState([
+    {
+      track: 'Enjoyt the Silence',
+      artist: 'Depeche Mode',
+      album: 'Violator',
+    },
+    {
+      trackName: 'Class Act, Final Front',
+      artist: 'Johny Rock',
+      album: 'Way Over There',
+    },
+  ]);
+
   return (
     <div className="App">
-      <p>Hello</p>
-      <SearchBar />
+      <h1>Results</h1>
+      <Track tracks={tracks}  />
     </div>
   );
 }
