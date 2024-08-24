625f26d672f98a751f043e689614a81a9cb9b163
asavickij <asavickij@gmail.com>
2024-08-24 09:04:50 +0300

    Set the logic in SearchResults.js

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Track from './Track';
 
-export default function SearchResults() {
+export default function SearchResults({ searchQuery, addTrackToTracklist }) {
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://locahost:5000/tracks?q=${searchQuery}');
+        const data = await response.json();
+        setSearchResults(data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    if (searchQuery) {
+      fetchData()
+    } else {
+      setSearchResults([]);
+    }
+
+  }, [searchQuery]);
+
   return (
     <div>
-      
+      {SearchResults.length > 0 ? (
+        <Track
+          tracks={searchResults}
+          addTrackToTracklist={addTrackToTracklist}
+        />
+      ) : (
+        <p>No results found.</p>
+      )}
     </div>
-  )
+  );
 }
