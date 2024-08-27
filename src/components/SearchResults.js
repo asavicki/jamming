3ddab65dc0b67ee8d89959eae1fdf7b502605f10
asavickij <asavickij@gmail.com>
2024-08-27 10:22:01 +0300

    Deleted commented block in Searchresults.js

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,9 +7,6 @@ export default function SearchResults({ searchResults, addTrackToTracklist }) {
   return (
     <div>
       <h2>Search results</h2>
-      {/* {searchQuery && searchResults.length === 0 && (
-        <p>No results found.</p>
-      )} */}
       {searchResults.length > 0 && (
         <Track
           tracks={searchResults}
