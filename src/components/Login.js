e08a1fe581e62be57ffeb53b74fd15285b614f29
asavickij <asavickij@gmail.com>
2024-08-28 15:21:26 +0300

    Added Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Login() {
+    const CLIENT_ID = '46d0dd945c24427b91fbadda22636b74';
+    const REDIRECT_URI = 'http://localhost:3000/';
+    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+    const RESPONSE_TYPE = 'token';
+
+    const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+
+    return (
+        <div>
+            <h1>Welcome to Jamming</h1>
+            <a href={loginUrl}>Log in</a>
+        </div>
+    );
+};
