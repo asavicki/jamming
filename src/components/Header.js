d7c40e2aaaf0095c554bbdf5cba2a13fbdf5a2d4
asavickij <asavickij@gmail.com>
2024-10-04 15:55:38 +0300

    Implemented countdown timer for token expiration.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../Styles.module.css';
 
 export default function Header({ logout }) {
+    const [timeLeft, setTimeLeft] = useState(59 * 60);
+    const [sessionExpired, setSessionExpired] = useState(false);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(prevTime => {
+                if (prevTime > 0) {
+                    return prevTime - 1;
+                } else {
+                    clearInterval(timer);
+                    setSessionExpired(true);
+                    return 0;
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return (
         <header>
             <div className={styles.app_name_container}>
                 <p>Jammming <br /><span>Spotify Playlist Creator</span></p>
             </div>
+            {sessionExpired ? (
+                <p className={styles.countdown}>Session has expired</p>
+            ) : (
+                <p className={styles.countdown}>Session expires in: {timeLeft}</p>
+            )}
             <button className={styles.logout_btn} onClick={logout}>Log Out</button>
         </header>
     )
